b8799c1702114f48339f0d18f340e69753242333
Kheang Senghorng <kheang.senghorng.2822@gmail.com>
2026-03-10 15:15:06 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/pay/success', [PayWayController::class,'success']);
 Route::get('/pay/cancel', [PayWayController::class,'cancel']);
 
 Route::post('/pay/callback', [PayWayController::class,'callback']);
+
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/test-email', function () {
+
+    Mail::raw('This is a test email from Laravel Fixit API.', function ($message) {
+        $message->to('senghorng0099p@gmail.com')
+                ->subject('Laravel Test Email');
+    });
+
+    return "Email sent!";
+});
